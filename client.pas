1be5e2f81a197393a98a8dc482cfd149deab0206
Nabuki <shichiromail@gmail.com>
2020-11-05 10:37:20 +0300

    all complete

---
+++ client.pas
@@ -0,0 +1,55 @@
+﻿//
+// CLIENT v1.0
+//
+
+program client;
+uses System.Net, System.Net.Sockets, System.Text;
+
+begin
+  try
+    var clnt := new TcpClient('127.0.0.1', 4156);
+    clnt.NoDelay := true;
+    
+    var stream := clnt.GetStream();
+    
+    var str_buff : array of byte;
+    SetLength(str_buff, 256);
+    
+    while true do
+    begin
+      if stream.dataAvailable then
+      begin
+        var readed := stream.Read(str_buff, 0, str_buff.Length);
+        var str := Encoding.UTF8.GetString(str_buff, 0, readed);
+        
+        case str[str.Length] of
+          #2: 
+          begin
+            write(str[1:str.Length]);
+          
+            var input : string;
+            readln(input);
+          
+            var input_buff := Encoding.UTF8.GetBytes(input + #3);
+            stream.Write(input_buff, 0, Length(input_buff));
+          end;
+          
+          #4:
+          begin
+            write(str[1:str.Length]);
+            break;
+          end;
+          
+          else write(str);
+        end;
+      end;
+    end;
+    
+    stream.close();
+    clnt.close();
+    
+  except on E: Exception do 
+    writeln(E.Message);
+  
+  end;
+end.
---
+++ server.pas
@@ -0,0 +1,122 @@
+﻿//
+// SERVER v1.0(SINGLE-THREADED)
+//
+
+program server;
+uses System.Net, System.Net.Sockets, System.Text;
+
+var stream: NetworkStream;
+
+procedure send(str: string); 
+begin
+  var buff := Encoding.UTF8.GetBytes(str);
+  stream.Write(buff, 0, Length(buff));
+end;
+
+procedure sendln(str: string);
+begin
+  send(str + #10);
+end;
+
+function recieve() : string;
+begin
+  stream.WriteByte(2);
+  
+  var buff : array of byte;
+  SetLength(buff, 256);
+  
+  var res := '';
+  while true do
+  begin
+    if stream.DataAvailable then
+    begin
+      var readed := stream.Read(buff, 0, buff.Length);
+      var str := Encoding.UTF8.GetString(buff, 0, readed);
+      
+      if str[str.Length] = #3 then
+      begin
+        res := res + str[1:str.Length];
+        break;
+      end
+      else
+        res := res + str;
+    end;
+  end;
+  
+  recieve := res;
+end;
+
+
+// <- начало пространства для записи решения
+function to_str(num: integer): string;
+begin
+  var res : string;
+  Str(num, res);
+  to_str := res;
+end;
+
+procedure send_task();
+begin
+  send('Введите целое четырехзначное число: ');
+  
+  while true do
+  begin
+    var str := recieve();
+    var num : integer;
+    var err : integer;
+    Val(str, num, err);
+    
+    if not (err = 0) then 
+    begin
+      send('Это точно не целое число, попробуйте снова: ');
+      continue;
+    end
+    else
+    begin
+      var digits := 0;
+      var sum := 0;
+      repeat
+        digits := digits + 1;
+        sum := sum + (num mod 10);
+        num := num div 10;
+      until num = 0;
+      
+      if not (digits = 4) then
+      begin
+        send('Введено не четырехзначное число, попробуйте снова: ');
+        continue;
+      end;
+      
+      sendln('Cумма цифр этого числа: ' + to_str(sum));
+      break;
+    end;
+  end;
+end;
+// <- конец пространства для записи решения
+
+
+begin
+  var srvr: TcpListener := new TcpListener(IPAddress.Any, 4156);
+  srvr.Start();
+  
+  while true do
+  begin
+    if srvr.Pending() then
+    begin
+      var connection := srvr.AcceptTcpClient();
+      connection.NoDelay := true;
+      
+      stream := connection.GetStream();
+      
+      try
+        send_task();
+        send(#4);
+      except on E: Exception do 
+        writeln(E.Message); 
+      end;
+      
+      stream.close();
+      connection.close();
+    end;
+  end;
+end.
